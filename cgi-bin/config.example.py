d80d5a033baff511f2aa767579137cf52fcead56
Chaobin Zhang <zhchbin@gmail.com>
2015-03-07 22:16:49 +0800

    :memo: Example config of db and avatar path.

--- cgi-bin/config.example.py
+++ cgi-bin/config.example.py
@@ -1,3 +1,9 @@
-MYSQL_USER = ''
+# MySQL user/password/db
+MYSQL_USER = 'yagra_user'
 MYSQL_PASSWORD = ''
-MYSQL_DB = ''
+MYSQL_DB = 'yagra'
+
+# Use for store user upload avatar files.
+#
+# Make sure user "www-data" can access this directory if you are using apache2.
+USER_AVATAR_DIRECTORY = '/var/www/yagra/avatar/'
